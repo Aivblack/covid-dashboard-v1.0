d18146a1784b9687c81ed8e1b7a7d7b35a0253e8
Adrian <aivillacorta.black@gmail.com>
2021-02-02 08:52:45 +0800

    Added chart

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
+// Variables
 const totalCases = document.getElementById('total-count');
 const activeCases = document.getElementById('active-count');
 const recoveries = document.getElementById('recoveries-count');
 const deaths = document.getElementById('deaths-count');
+
 
 fetch('https://corona-api.com/countries')
   .then(response => response.json())
@@ -16,6 +18,45 @@ fetch('https://corona-api.com/countries')
       let dateCut = dateSlice.slice(0, 10);
       document.getElementById('update').innerText = dateCut;
 
+      // Chart
+      let totalChart = myData.data[231].latest_data.confirmed;
+      let activeChart = myData.data[231].latest_data.critical;
+      let recoveriesChart = myData.data[231].latest_data.recovered;
+      let deathsChart = myData.data[231].latest_data.deaths;
+
+      Chart.defaults.global.defaultFontFamily = 'Montserrat';
+
+      let myChart = document.getElementById('myChart').getContext('2d');
+      let chart = new Chart(myChart, {
+        type: 'bar',
+        data: {
+          labels: ['Total cases', 'Active cases', 'Recoveries', 'Deaths'],
+          datasets: [{
+            label: 'Count',
+            data: [
+              totalChart,
+              activeChart,
+              recoveriesChart,
+              deathsChart
+            ],
+            backgroundColor:[
+              'blue',
+              'red',
+              'green',
+              'black'
+            ],
+            hoverBorderWidth: 4,
+            hoverBorderColor: 'black'
+          }]
+        },
+        options: {
+          legend: {
+            display: false
+          }
+        }
+      });
   });
+
+
 
 
